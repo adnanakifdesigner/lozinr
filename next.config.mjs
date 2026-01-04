3ffba601cc66952af20af3045e5d070ac620c0d0
adnan <adnanakifsocial@gmail.com>
2026-01-04 08:37:27 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactCompiler: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
   images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      { hostname: "*.public.blob.vercel-storage.com" },
-      { hostname: "tkyy9tub5efxttep.public.blob.vercel-storage.com" },
-    ],
+    unoptimized: true,
   },
-  compress: true,
-}
+};
 
-export default nextConfig
+export default nextConfig;
