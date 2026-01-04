609abb372277fefcb02ade73bc65b3a6ef7cc4aa
adnan <adnanakifsocial@gmail.com>
2026-01-04 08:51:27 +0600

    Update next.config.mjs for image formats and TypeScript

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
+  // reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
-};
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      { hostname: "*.public.blob.vercel-storage.com" },
+      { hostname: "tkyy9tub5efxttep.public.blob.vercel-storage.com" },
+    ],
+  },
+  compress: true,
+}
 
-export default nextConfig;
+export default nextConfig
